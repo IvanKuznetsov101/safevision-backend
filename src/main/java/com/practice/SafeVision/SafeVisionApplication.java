1b3fe3bc2812f384bb9b450a7f184676c19cef4f
Kuznetsov Ivan <kuznetsov1637@gmail.com>
2025-05-03 18:52:08 +0300

    initial

---
+++ src/main/java/com/practice/SafeVision/SafeVisionApplication.java
@@ -0,0 +1,13 @@
+package com.practice.SafeVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/SafeVision/SafeVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SafeVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
